88fa2a8aa90a0a464c9449b021abeabdf65a2ae9
jastrab <vladimir.durech@brainit.sk>
2024-04-30 17:54:20 +0200

    1.1.0 - podpora Kodi 19 - pridane vyhladavanie (by jastrab) - oprava stahovania (by jastrab) - oprava prehravania  (by jastrab)

--- resources/lib/zkouknito.py
+++ resources/lib/zkouknito.py
@@ -20,30 +20,36 @@
 # *
 # */
 
-import urllib2,re,os,cookielib
-import util,resolver
-
+import urllib, re, os
+import util, resolver
+import http.cookiejar
 from provider import ContentProvider
+
+def get_url(**kwargs):
+		return '{0}?{1}'.format(_url, urlencode(kwargs, 'utf-8'))
+
 
 class ZkouknitoContentProvider(ContentProvider):
 
 	def __init__(self,username=None,password=None,filter=None):
 		ContentProvider.__init__(self,'zkouknito.cz','http://zkouknito.cz/',username,password,filter)
-		ck = cookielib.Cookie(version=0, name='confirmed', value='1', port=None, port_specified=False,
+		ck = http.cookiejar.Cookie(version=0, name='confirmed', value='1', port=None, port_specified=False,
 										 domain='zkouknito.cz', domain_specified=True, domain_initial_dot=False, 
 										 path='/', path_specified=True, secure=False, expires=None, discard=True, 
 										 comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
-		cj = cookielib.LWPCookieJar()
+		cj = http.cookiejar.LWPCookieJar()
 		cj.set_cookie(ck)
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-		urllib2.install_opener(opener)
+		opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+		urllib.request.install_opener(opener)
 
 	def capabilities(self):
-		return ['resolve','categories']
+		return ['resolve','categories', 'search']
 
 
 	def categories(self):
 		result = []
+		# self.info ('== categories ==')
+		# self.info ( self.base_url )
 		data = util.substr(util.request(self.base_url+'videa'),'<ul class=\"category','</ul')
 	#	util.add_dir('Online TV',{'cat':'http://www.zkouknito.cz/online-tv'})
 		pattern='<a href=\"(?P<url>[^\"]+)[^>]+>(?P<cat>[^<]+)</a>'	
@@ -53,50 +59,83 @@ class ZkouknitoContentProvider(ContentProvider):
 			item = self.dir_item()
 			item['title'] = m.group('cat')
 			item['url'] = m.group('url')
+			# self.info (item)
 			result.append(item)
+
 		return result
-		
+
+	def search(self, keyword):
+			# keyword = get_user_input()
+			# return ('what='+urllib.parse.quote(keyword))
+			return self.list('https://www.zkouknito.cz/hledej?string=%s'%urllib.parse.quote(keyword))
+
 	def list(self,url):
 		result = []
+		# self.info ('== list ==')
+		# self.info ( url )
 		url = self._url(url)
+		if '#search#' in url:
+			url = search()
+
 		page = util.request(url)
-		q = url.find('?')
-		if q > 0:
-			url = url[:q]
-		data = util.substr(page,'<div id=\"videolist','<div class=\"paging-adfox\">')
+		if '/hledej?string' in url:
+			# q = url.find('?')
+			# if q > 0:
+			# 	url = url[:q]
+			data = util.substr(page,'</a> &gt; Vyhledávání</p>','<table class=\"insertedAd\">')
+		else:
+			data = util.substr(page,'<div id=\"videolist','<div class=\"paging-adfox\">')
+
 		#pattern='<div class=\"img-wrapper\"><a href=\"(?P<url>[^\"]+)\" title=\"(?P<name>[^\"]+)(.+?)<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<p class=\"dsc\">(?P<plot>[^<]+)'
 		pattern='<div class=\"img-wrapper\"><a href=\"(?P<url>[^\"]+)\" title=\"(?P<name>[^\"]+)(.+?)<img(.+?)src=\"(?P<img>[^\"]+)(.+?)'
 		for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
 			item = self.video_item()
 			item['title'] = m.group('name')
-			item['url'] = m.group('url')
+			iurl = m.group('url')
+			#item['url'] = re.sub('\\\\/', '\\\\', m.group('url'))
+			iurl = re.sub('\\\\/', '/', iurl)
+			item['url'] = iurl
 			item['img'] = m.group('img')
+			# self.info ( item )
 			#item['plot'] = m.group('plot')
 			self._filter(result,item)
 
 		data = util.substr(page,'<div class=\"jumpto\"','</div>')
 	
 		data = util.substr(page,'<p class=\"paging','</p>')
-		next = re.search('<a href=\"(?P<url>[^\"]+)\"><img(.+?)next\.png',data,re.IGNORECASE | re.DOTALL)
+		# self.info ( data )
+		#next = re.search('<a href=\"(?P<url>[^\"]+)\"><img(.+?)next\.png',data,re.IGNORECASE | re.DOTALL)
+		next = re.search('<a class=\"next\" href=\"(?P<url>[^\"]+)\">Zobrazit',data,re.IGNORECASE | re.DOTALL)
+		# self.info ( next )
 		if next:
 			item = self.dir_item()
 			item['type'] = 'next'
 			item['url'] = url+next.group('url')
 			result.append(item)
+
+		item = self.dir_item()
+		item['title'] = 'Search'
+		item['url'] = '#search#'
+		item['menu'] = {'$30070':{'list':item['url'], 'action-type':'list'}}
 		return result
 		
 	def resolve(self,item,captcha_cb=None,select_cb=None):
 		item = item.copy()
 		url = self._url(item['url'])
+		# self.info ('resolve == ' + url )
 		data = util.request(url)
-		resolved = resolver.findstreams(data,["\'file\'\:.?\'(?P<url>[^\']+)"])
+		resolved = resolver.findstreams(data,["{\"src\":\"(?P<url>[^\"]+).+?poster = \"(?P<poster>[^\"]+).+"])
 		result = []
 		for i in resolved:
 			item = self.video_item()
 			item['title'] = i['name']
-			item['url'] = i['url']
+			iurl = i['url']
+			# iurl = re.sub('\\\\/', '\\\\', iurl)
+			iurl = re.sub('\\\\/', '/', iurl)
+			item['url'] = iurl
 			item['quality'] = i['quality']
 			item['surl'] = i['surl']
+			# self.info ( item )
 			result.append(item)     
 		if len(result)==1:
 			return result[0]
